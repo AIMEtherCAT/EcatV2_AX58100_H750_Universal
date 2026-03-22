4a649a0329a9af5613657a85fba71696a4954d79
Animex77 <Animex2077@outlook.com>
2026-03-23 01:50:22 +0800

    Add: VT13 USART Remote control receive and checksum task Typo fix: "CHANNAL" to "CHANNEL"

---
+++ Application/Task/vt13_rc.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Xiuqi Wang on 2026/3/23.
+//
+#include "buffer_utils.hpp"
+#include "main.h"
+#include "peripheral_utils.hpp"
+#include "task_defs.hpp"
+#include "crc_utils.hpp"
+
+namespace aim::ecat::task::vt13_rc {
+  VT13_RC::VT13_RC(buffer::Buffer * /* buffer */) : UartRunnable(false, TaskType::VT13_RC) {
+    init_peripheral(peripheral::Type::PERIPHERAL_USART1);
+
+    get_peripheral<peripheral::UartPeripheral>()->receive_by_dma(32);
+  }
+
+  void VT13_RC::write_to_master(buffer::Buffer *slave_to_master_buf) {
+    uint8_t report_buf[RC_MSG_PKG_LEN] = {};
+    buf_.read(report_buf, RC_MSG_PKG_LEN);
+    slave_to_master_buf->write(report_buf, RC_MSG_PKG_LEN);
+    slave_to_master_buf->write_uint8(buffer::EndianType::LITTLE, HAL_GetTick() - last_receive_time_.get() <= 20);
+  }
+
+  void VT13_RC::uart_recv(const uint16_t size) {
+    auto pUart1Buffer = get_peripheral<peripheral::UartPeripheral>()->recv_buf_->get_buf_pointer<uint8_t>();
+    if (pUart1Buffer[0] == 0xA9 && pUart1Buffer[1] == 0x53 && size == RC_FULL_PKG_LEN) {
+      if (algorithm::crc16::verify_CRC16_check_sum(pUart1Buffer,RC_FULL_PKG_LEN)) {
+        uint8_t recv_buf[RC_MSG_PKG_LEN] = {};
+        get_peripheral<peripheral::UartPeripheral>()->recv_buf_->raw_read(recv_buf, RC_MSG_PKG_LEN);
+        buf_.write(recv_buf, RC_MSG_PKG_LEN);
+      }
+    }
+    get_peripheral<peripheral::UartPeripheral>()->receive_by_dma(32);
+  }
+
+  void VT13_RC::uart_err() {
+    get_peripheral<peripheral::UartPeripheral>()->receive_by_dma(32);
+  }
+
+  void VT13_RC::exit() {
+    get_peripheral()->deinit();
+  }
+}
